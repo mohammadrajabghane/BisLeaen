2b2cb330e17adaa746010369da95a1da6e4a0586
Hossein Rafee <hossein.rafee1998@gmail.com>
2020-09-04 01:18:58 +0430

    add files

---
+++ dist/js/Change_Icon.js
@@ -0,0 +1,59 @@
+//change icon when hover
+function hovermyaccount() {
+  var x = document.getElementById("my-account_icon");
+  x.setAttribute("src", "./../../image/usreG.png");
+}
+
+function unhovermyaccount() {
+  var x = document.getElementById("my-account_icon");
+  x.setAttribute("src", "./../../image/user2.png");
+}
+
+function hovercheck() {
+  var x = document.getElementById("check_icon");
+  x.setAttribute("src", "./../../image/chekG.png");
+}
+
+function unhovercheck() {
+  var x = document.getElementById("check_icon");
+  x.setAttribute("src", "./../../image/check (1).png");
+}
+function hoverspeech() {
+  var x = document.getElementById("speech_icon");
+  x.setAttribute("src", "./../../image/speechG.png");
+}
+
+function unhoverspeech() {
+  var x = document.getElementById("speech_icon");
+  x.setAttribute("src", "./../../image/speech.png");
+}
+function hoverbox() {
+  var x = document.getElementById("box_icon");
+  x.setAttribute("src", "./../../image/boxG.png");
+}
+
+function unhoverbox() {
+  var x = document.getElementById("box_icon");
+  x.setAttribute("src", "./../../image/box.png");
+}
+
+function hoverwriting() {
+  var x = document.getElementById("writing_icon");
+  x.setAttribute("src", "./../../image/writingG.png");
+}
+
+function unhoverwriting() {
+  var x = document.getElementById("writing_icon");
+  x.setAttribute("src", "./../../image/writing.png");
+}
+function hoverlogout() {
+  var x = document.getElementById("logout_icon");
+  x.setAttribute("src", "./../../image/logoutG.png");
+}
+
+function unhoverlogout() {
+  var x = document.getElementById("logout_icon");
+  x.setAttribute("src", "./../../image/logout.png");
+}
+
+// end change icon when hover
